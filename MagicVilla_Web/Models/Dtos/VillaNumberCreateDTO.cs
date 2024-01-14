87d44e96a45830df3dda8459ae0d7f63c70e831d
Hazhir2002 <HazhirYousefi2002@gmail.com>
2024-01-14 15:51:34 +0330

    Adding the API end point Service

---
+++ MagicVilla_Web/Models/Dtos/VillaNumberCreateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Dtos
+{
+    public class VillaNumberCreateDTO
+    {
+        [Required]
+        public int VillaNum { get; set; }
+
+        [Required]
+        public int VillaID { get; set; }
+
+        public string SpecialDetails { get; set; }
+    }
+}
---
+++ MagicVilla_Web/Models/Dtos/VillaNumberDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Web.Dtos
+{
+    public class VillaNumberDTO
+    {
+        [Required]
+        public int VillaNum { get; set; }
+
+        [Required]
+        public int VillaID { get; set; }
+
+        public string SpecialDetails { get; set; }
+    }
+}
